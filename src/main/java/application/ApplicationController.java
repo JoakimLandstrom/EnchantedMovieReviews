f6ac5e211fcdd687356f3f317258f6ba8ad7cbea
JoakimLandstrom <jockez@live.com>
2016-02-20 18:06:56 +0100

    We did it!

---
+++ src/main/java/application/ApplicationController.java
@@ -0,0 +1,15 @@
+package application;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+
+	@RequestMapping("/")
+	public String index(){
+		return "Hello world!";
+		
+	}
+	
+}
